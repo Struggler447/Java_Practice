7475731934943296cc51d83154cea2226383b0b6
struggler447 <triveshraykrishna@gmail.com>
2022-06-22 08:11:47 +0530

    First commit

---
+++ Firstprogram.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Firstprogram {
+	int x;
+	
+	void number() {
+		System.out.println(x);
+		int x = 1;
+	}
+	
+	public static void main(String[] args) {
+		Firstprogram first = new Firstprogram();
+		first.number();
+	}
+}
